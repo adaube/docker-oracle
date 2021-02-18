b85306d877c6f227c3bc16c7f73662c0eff4e1f8
Adam Daube <adam.daube.ad@gmail.com>
2021-02-18 14:44:22 -0500

    simpler xe

--- resetOracleConfiguration.sh
+++ resetOracleConfiguration.sh
@@ -6,14 +6,14 @@ sudo rm -rf ~/.oradata
 # create directory
 mkdir ~/.oradata
 
-# set permissions (oracle in container)
+# set permissions, oracle in container
 sudo chown 54321:54321 ~/.oradata
+# give write permissions
+sudo chmod -R o+w ~/.oradata
 
 # start container
 docker run --rm \
   --name oracle-xe \
-  --shm-size=1g \
   -p 1521:1521 -p 5500:5500 \
-  -e ORACLE_SID=ORCLCDB -e ORACLE_PDB=XE -e ORACLE_PWD=oracle -e ORACLE_CHARACTERSET=AL32UTF8 \
   -v $HOME/.oradata:/opt/oracle/oradata \
   oracle/database:18.4.0-xe
--- startOracle.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# start container
-docker run --rm --name oracle-xe \
-  --shm-size=1g \
-  -p 1521:1521 -p 5500:5500 \
-  -e ORACLE_SID=ORCLCDB -e ORACLE_PDB=XE -e ORACLE_PWD=oracle -e ORACLE_CHARACTERSET=AL32UTF8 \
-  -v $HOME/.oradata:/opt/oracle/oradata \
-  oracle/database:18.4.0-xe
