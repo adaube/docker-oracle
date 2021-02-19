656cc1dedad621efaa13bbe22e800c366d307fc8
Adam Daube <adam.daube.ad@gmail.com>
2021-02-19 09:45:40 -0500

    trim down create

--- createOracleDocker.sh
+++ createOracleDocker.sh
@@ -8,14 +8,3 @@ git clone https://github.com/oracle/docker-images.git
 
 # run docker image build script (relative to git repo root)
 ./docker-images/OracleDatabase/SingleInstance/dockerfiles/buildDockerImage.sh -v 18.4.0 -x
-
-# data persistance
-mkdir -p $HOME/.oradata
-# this is uid of oracle (inside the container)
-sudo chown 54321:54321 ~/.oradata
-
-# import database dump
-# docker exec -it oracle-ee impdp \'/ as sysdba\' schemas=NYDATA exclude=constraint, ref_constraint, index TABLE_EXISTS_ACTION=replace directory=impdp_dump_dir dumpfile=16_FLEXCUBE.dmp
-# docker exec -it oracle-ee impdp \'/ as sysdba\' schemas=NYDATA exclude=constraint, ref_constraint, index TABLE_EXISTS_ACTION=append directory=impdp_dump_dir dumpfile=16_FLEXCUBE.dmp
-# docker exec -it oracle-ee impdp \'/ as sysdba\' schemas=NYDATA exclude=constraint, ref_constraint, index directory=impdp_dump_dir dumpfile=20_FLEXCUBE.dmp
-# docker exec -it oracle-ee imp \'/ as sysdba\' full=Y file=/opt/oracle/oradata/20_ULTIMUS_NYDATA.dmp
