c2ef8f3032f8e451f24a7d5b4995a5d9840850fc
Adam Daube <adam.daube.ad@gmail.com>
2021-03-01 14:01:51 -0500

    next steps

--- startEnterpriseOracle.sh
+++ startEnterpriseOracle.sh
@@ -11,12 +11,13 @@ sudo chown 54321:54321 ~/.oradata
 # give write permissions
 sudo chmod -R o+w ~/.oradata
 
-# start container
+# start container (ee)
 docker run \
   --name oracle-ee \
   -p 1521:1521 \
-  -e ORACLE_SID=orcl \
-  -e ORACLE_PDB=xe \
+  -p 5500:5500 \
+  -e ORACLE_SID=ORCLCDB \
+  -e ORACLE_PDB=ORCLPDB1 \
   -e ORACLE_PWD=oracle \
   -e ORACLE_CHARACTERSET=WE8MSWIN1252 \
   -v $HOME/.oradata:/opt/oracle/oradata \
---
+++ startOracleSE.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# connection url: jdbc:oracle:thin:@localhost:1521/ORCLPDB1?oracle.net.disableOob=true
+DIRECTORY=`dirname $0`
+DIRECTORY=$(realpath $DIRECTORY)
+
+docker run \
+  --name oracle-se2 \
+  -p 1521:1521 \
+  -p 5500:5500 \
+  --shm-size=1g \
+  -v ${DIRECTORY}/sql:/docker-entrypoint-initdb.d/setup \
+  -d oracle/database:19.3.0-se2
