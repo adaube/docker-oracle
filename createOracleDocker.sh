402a228df9fad44199a80f6bb3a2d301e232fc48
Adam Daube <adam.daube.ad@gmail.com>
2021-03-02 10:14:47 -0500

    update create

--- createOracleDocker.sh
+++ createOracleDocker.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-# clone the oracle docker image repo
-git clone https://github.com/oracle/docker-images.git
+# 1. clone the oracle docker image repo (git clone https://github.com/oracle/docker-images.git)
 
-# download oracle 18c binaries into the build location (requires oracle account)
-# ~/projects/docker-images/OracleDatabase/SingleInstance/dockerfiles/18.4.0/oracle-database-xe-18c-1.0-1.x86_64.rpm
+# 2. download oracle 19c binaries into the build location (requires oracle account)
 
-# run docker image build script (relative to git repo root)
-./docker-images/OracleDatabase/SingleInstance/dockerfiles/buildDockerImage.sh -v 18.4.0 -x
+# 3. run docker image build script (relative to git repo root)
+~/projects/docker-images/OracleDatabase/SingleInstance/dockerfiles/buildContainerImage.sh -v 19.3.0 -e
