655a3f635ad234dd75833a06639b250712c699bd
Adam Daube <adam.daube.ad@gmail.com>
2021-03-02 10:09:47 -0500

    auto password generate

--- enterpriseOracleStart.sh
+++ enterpriseOracleStart.sh
@@ -8,9 +8,8 @@ docker run --rm \
   --name oracle-ee \
   -p 1521:1521 \
   -p 5500:5500 \
-  -e ORACLE_SID=ORCLCDB \
-  -e ORACLE_PDB=ORCLPDB1 \
-  -e ORACLE_PWD=oracle \
   -e ORACLE_CHARACTERSET=WE8MSWIN1252 \
+  --shm-size=1g \
   -v $HOME/.oradata:/opt/oracle/oradata \
+  -v ${DIRECTORY}/sql:/docker-entrypoint-initdb.d/setup \
   oracle/database:19.3.0-ee
